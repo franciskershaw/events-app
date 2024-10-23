e0a0d32489d489f40fba04f9ef58017de2ab7bcd
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-10-23 21:58:24 +0100

    Add some routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+// import PrivateRoute from "./components/layout/PrivateRoute/PrivateRoute";
+import SharedLayout from "./components/layout/SharedLayout/SharedLayout";
 
 function App() {
-  return <div>Organisy</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<div>Organisy</div>} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
