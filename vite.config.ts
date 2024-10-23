a4f392845564e02d524dd6ed02f18cd2a4219743
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-10-23 17:00:18 +0100

    Clean out boilerplate

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: "http://localhost:5173",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
