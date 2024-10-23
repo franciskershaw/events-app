d1f65f410379cf0d1e5d68665c65a1f85448db14
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-10-23 21:23:16 +0100

    Add sonner and react query

---
+++ src/tanstackQuery/TanstackProvider.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from "react";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+});
+
+const TanstackProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default TanstackProvider;
