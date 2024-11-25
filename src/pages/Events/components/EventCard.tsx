c4773b571b2d8427ec406a9d24f98b3b8a338c08
zoet24 <zoethexton.dev@gmail.com>
2024-11-25 20:54:30 +0000

    Fine tune date and time formatting functions

--- src/pages/Events/components/EventCard.tsx
+++ src/pages/Events/components/EventCard.tsx
@@ -1,6 +1,8 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
+import { Button } from "../../../components/ui/button";
 import useFormattedDate from "../../../hooks/utility/useFormattedDate";
+import useFormattedTime from "../../../hooks/utility/useFormattedTime";
 import { EventCategory, EventDate } from "../../../types/globalTypes";
 
 export interface EventCardProps {
@@ -13,15 +15,26 @@ export interface EventCardProps {
     createdBy: string;
     sharedWith: string[];
     createdAt: string;
-    updatedAt: string;
+    // extraInfo?: string;
   };
 }
 
 const EventCard = ({ event }: EventCardProps) => {
+  const {
+    location,
+    title,
+    category,
+    createdBy,
+    sharedWith,
+    createdAt,
+    // extraInfo,
+  } = event;
+
   const [isOpen, setIsOpen] = useState(false);
   const toggleBody = () => setIsOpen((prev) => !prev);
 
   const date = useFormattedDate(event.date);
+  const time = useFormattedTime(event.date);
 
   return (
     <div className="event-card">
@@ -35,9 +48,8 @@ const EventCard = ({ event }: EventCardProps) => {
         <div className="box p-1 rounded-md">
           <span>{date}</span>
         </div>
-        {/* <h2>{event.title}</h2> */}
-        <h2>Francis I love you!!</h2>
-        {/* <div>{event.location}</div> */}
+        <h2>{title}</h2>
+        {/* <div>{location}</div> */}
       </div>
       <motion.div
         className="event-card-body box mx-2 border-t-0 rounded-md rounded-t-none overflow-hidden"
@@ -48,7 +60,21 @@ const EventCard = ({ event }: EventCardProps) => {
           opacity: { duration: isOpen ? 0.1 : 0.3 },
         }}
       >
-        <h1 className="text-9xl">HELLO</h1>
+        <div className="p-2 space-y-2">
+          {/* Meta */}
+          <div className="flex space-x-2">
+            <div className="box rounded-md p-1">{time}</div>
+            <div className="box rounded-md p-1">Event categories</div>
+          </div>
+          {/* Description */}
+          {/* {extraInfo && <div className="box rounded-md">{extraInfo}</div>} */}
+          {/* Buttons */}
+          <div className="flex justify-center space-x-2">
+            <Button size="round">Edit</Button>
+            <Button size="round">Delete</Button>
+            <Button size="round">Private</Button>
+          </div>
+        </div>
       </motion.div>
     </div>
   );
