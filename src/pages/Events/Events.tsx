7abd16292c0b0daffdcb2d82544f4229061024ff
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-12-07 22:34:58 +0000

    Fix events rendering

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -3,26 +3,19 @@ import useGetEvents from "./hooks/useGetEvents";
 
 const Events = () => {
   const { events, fetchingEvents, errorFetchingEvents } = useGetEvents();
-  const {
-    events: pastEvents,
-    fetchingEvents: fetchingPastEvents,
-    errorFetchingEvents: errorFetchingPastEvents,
-  } = useGetEvents();
 
-  if (fetchingEvents || fetchingPastEvents) return <p>Loading events...</p>;
-  if (errorFetchingEvents || errorFetchingPastEvents)
-    return <p>Error fetching events</p>;
+  if (fetchingEvents) return <p>Loading events...</p>;
+  if (errorFetchingEvents) return <p>Error fetching events</p>;
 
   // TOZO: Refactor to populate empty event cards within time period
 
   return (
     <>
-      {events.length === 0 && pastEvents.length === 0 ? (
+      {events.length === 0 ? (
         <p>No events found.</p>
       ) : (
         <>
           <div className="top-0 left-0 right-0 fixed h-[60px] bg-white/90 z-10"></div>
-          {pastEvents.length > 0 && <EventCards events={pastEvents} />}
           {events.length > 0 && <EventCards events={events} />}
         </>
       )}
