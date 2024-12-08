ab5779a83e72e053242a42f2d1d61ec5f67bbca3
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-12-08 15:35:34 +0000

    Add event category select options too hook

--- src/pages/Events/hooks/useGetEventCategories.tsx
+++ src/pages/Events/hooks/useGetEventCategories.tsx
@@ -1,8 +1,11 @@
+import { useMemo } from "react";
+
 import { useQuery } from "@tanstack/react-query";
 
 import useAxios from "@/axios/useAxios";
 import useUser from "@/hooks/user/useUser";
 import queryKeys from "@/tanstackQuery/queryKeys";
+import { EventCategory } from "@/types/globalTypes";
 
 const useGetEventCategories = () => {
   const api = useAxios();
@@ -16,13 +19,21 @@ const useGetEventCategories = () => {
   };
 
   const { data: eventCategories = [], isFetching: fetchingEventCategories } =
-    useQuery({
+    useQuery<EventCategory[]>({
       queryKey: [queryKeys.eventCategories],
       queryFn: getEventCategories,
     });
 
+  const eventCategorySelectOptions = useMemo(() => {
+    return eventCategories.map((category) => ({
+      label: category.name,
+      value: category._id,
+    }));
+  }, [eventCategories]);
+
   return {
     eventCategories,
+    eventCategorySelectOptions,
     fetchingEventCategories,
   };
 };
