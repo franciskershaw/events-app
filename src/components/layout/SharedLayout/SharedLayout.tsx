2059ba73390c5faa80f80b063a56110a3be1b4e9
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-12-08 16:18:39 +0000

    Remove event modal from login page

--- src/components/layout/SharedLayout/SharedLayout.tsx
+++ src/components/layout/SharedLayout/SharedLayout.tsx
@@ -4,12 +4,14 @@ import { Outlet } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Toaster } from "@/components/ui/sonner";
 import { useModals } from "@/contexts/ModalsContext";
+import useUser from "@/hooks/user/useUser";
 import AddEventModal from "@/pages/Events/components/AddEventModal";
 
 const SharedLayout = () => {
   // const { logout } = useAuth();
   const { openEventModal, isEventModalOpen, closeModal, selectedEvent } =
     useModals();
+  const { user } = useUser();
   return (
     <>
       <div className="min-h-screen">
@@ -22,16 +24,19 @@ const SharedLayout = () => {
         {/* <nav className="box fixed bottom-0 left-0 right-0 bg-white">
         Bottom nav
       </nav> */}
-        <Button onClick={() => openEventModal()}>Add event</Button>
+        {user && <Button onClick={() => openEventModal()}>Add event</Button>}
         <Toaster />
       </div>
-      <AddEventModal
-        open={isEventModalOpen}
-        onOpenChange={(open) =>
-          open && selectedEvent ? openEventModal(selectedEvent) : closeModal()
-        }
-        closeModal={closeModal}
-      />
+
+      {user && (
+        <AddEventModal
+          open={isEventModalOpen}
+          onOpenChange={(open) =>
+            open && selectedEvent ? openEventModal(selectedEvent) : closeModal()
+          }
+          closeModal={closeModal}
+        />
+      )}
     </>
   );
 };
