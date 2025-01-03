c43d8b53906a6b4da51036d216928a10f3c69c24
zoet24 <zoethexton.dev@gmail.com>
2025-01-03 17:53:59 +0000

    Fiiiixed it, sorry sorry sorry

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import React from "react";
 
 import EventsNavbarTop from "../../components/layout/navigation/EventsNavbarTop/EventsNavbarTop";
 import {
@@ -9,13 +9,12 @@ import EventCards from "./components/EventCards";
 import useGetEventCategories from "./hooks/useGetEventCategories";
 import useGetEvents from "./hooks/useGetEvents";
 
-export const Events = () => {
+const Events = () => {
   const { events } = useGetEvents();
   const { eventCategorySelectOptions } = useGetEventCategories();
-  const initialEvents = useMemo(() => events || [], [events]);
-  const { query, setQuery, filteredEvents } = useSearch();
+  const initialEvents = React.useMemo(() => events || [], [events]);
 
-  const categories = useMemo(
+  const categories = React.useMemo(
     () =>
       eventCategorySelectOptions.map((category) => ({
         _id: { $oid: category.value },
@@ -26,10 +25,22 @@ export const Events = () => {
 
   return (
     <SearchProvider initialEvents={initialEvents} categories={categories}>
+      <EventsWithSearch />
+    </SearchProvider>
+  );
+};
+
+const EventsWithSearch = () => {
+  const { query, setQuery, filteredEvents } = useSearch();
+
+  return (
+    <>
       <EventsNavbarTop query={query} setQuery={setQuery} />
       <div className="p-4">
         {filteredEvents.length === 0 ? <p>No events found.</p> : <EventCards />}
       </div>
-    </SearchProvider>
+    </>
   );
 };
+
+export default Events;
