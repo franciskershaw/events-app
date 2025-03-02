678d93fb06726e0599955f26aafc07382db46b05
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2025-03-02 18:45:04 +0000

    Tweaks to some of the icons

--- src/lib/icons.tsx
+++ src/lib/icons.tsx
@@ -1,32 +1,39 @@
 import {
   FaBeer,
+  FaBell,
   FaBirthdayCake,
   FaBriefcase,
   FaCalendarAlt,
+  FaCalendarDay,
   FaChurch,
   FaFutbol,
   FaGlassCheers,
   FaMusic,
   FaPlane,
+  FaRunning,
   FaTrain,
   FaUserMd,
   FaUtensils,
 } from "react-icons/fa";
+import { FaTent } from "react-icons/fa6";
 
 // TODO: Change data structure of categories - no need to have icons, just map against name?
 // TODO: Other ideas for categories - coffee
 const categoryIcons: Record<string, JSX.Element> = {
+  Activity: <FaRunning />,
+  Anniversary: <FaCalendarDay />,
   Appointment: <FaUserMd />,
   Birthday: <FaBirthdayCake />,
   Dinner: <FaUtensils />,
   Drinks: <FaBeer />,
-  Festival: <FaMusic />,
+  Festival: <FaTent />,
   Gig: <FaMusic />,
   Holiday: <FaPlane />,
   "In Transit": <FaTrain />,
   "Night Out": <FaGlassCheers />,
   Other: <FaCalendarAlt />,
   Party: <FaBirthdayCake />,
+  Reminder: <FaBell />,
   Sport: <FaFutbol />,
   Wedding: <FaChurch />,
   Work: <FaBriefcase />,
